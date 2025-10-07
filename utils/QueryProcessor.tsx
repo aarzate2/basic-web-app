ae11f68b9bde6c1c7b0516b11b880684872fb1b3
aarzate2 <aarzate@andrew.cmu.edu>
2025-10-07 09:58:56 -0400

    name fix

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "aarzate";
   }
   if (query.toLowerCase().includes("andrewid")) {
     return "aarzate";
